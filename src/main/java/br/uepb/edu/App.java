0d7d055eb7afcfa2df7448e62d2a4b7c1e00a0f0
Rodrigo Loiola <59403248+rodloiola@users.noreply.github.com>
2023-10-07 20:08:25 -0300

    Add files via upload

---
+++ src/main/java/br/uepb/edu/App.java
@@ -0,0 +1,12 @@
+package br.uepb.edu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/br/uepb/edu/domain/Leitor.java
@@ -0,0 +1,16 @@
+package br.uepb.edu.domain;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Leitor {
+    private String nome;
+    private LeitorTipo tipo;
+    private List<Reserva> reservas;
+}
