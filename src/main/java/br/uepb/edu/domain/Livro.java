5ea2d8a78c891c286af6721db79a16aca2373c9a
DenisWMS <131109136+DenisWMS@users.noreply.github.com>
2023-10-09 17:08:59 -0300

    Organizando

--- src/main/java/br/uepb/edu/domain/Livro.java
+++ src/main/java/br/uepb/edu/domain/Livro.java
@@ -7,11 +7,18 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Livro {
+public class Livro implements ItemDeBiblioteca {
     private String titulo;
     private String autor;
     private int ano;
     private String isbn;
     private String editora;
     private LivroTipo tipo;
+
+    // Implementação do método getDescricao da interface ItemDeBiblioteca
+    @Override
+    public String getDescricao() {
+        // Retorna uma descrição formatada do livro com título e autor
+        return "Livro: " + titulo + " de " + autor;
+    }
 }
